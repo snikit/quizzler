55d3ac9a427943ac66de7fcef93e30aab4cfa1ca
Abhishek Thakur <abhishek.thakur@geminisolutions.in>
2021-01-02 21:55:12 +0530

    more admin

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [{ path: 'candidate', loadChildren: () => import('./candidate/candidate.module').then(m => m.CandidateModule) }];
+const routes: Routes = [
+  {
+    path: 'candidate',
+    loadChildren: () =>
+      import('./candidate/candidate.module').then((m) => m.CandidateModule),
+  },
+  {
+    path: 'admin',
+    loadChildren: () =>
+      import('./admin/admin.module').then((m) => m.AdminModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
